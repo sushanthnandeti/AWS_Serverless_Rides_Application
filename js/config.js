a946c5cd60392e8b07355c5488bf0c5e7dde4abd
Sushanth N <sushanthnandeti96@gmail.com>
2023-09-25 19:54:57 -0400

    add region to js.config file

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'us-east-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
