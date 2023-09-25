c7dbe917f50136f62f55577b069eb066c98ea86b
Sushanth N <sushanthnandeti96@gmail.com>
2023-09-25 19:48:23 -0400

    new_config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_vyG0WzMQf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1nmsadj5el2si0c9ms4q354vfc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
